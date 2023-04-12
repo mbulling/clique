68820465df9eeec5629f5ce4496ad7af97cd171d
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-04-12 14:34:33 -0400

    user implemented

--- lib/backend/user.dart
+++ lib/backend/user.dart
@@ -1,2 +1,21 @@
-// check local storage for static userIdentity (6 digit number)
-// if not found, generate a new one and save it to local storage
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:math';
+
+// Generate user identifier
+String generateUserIdentity() {
+  Random random = Random();
+  return (random.nextInt(999999)).toString();
+}
+
+// Retrieve the userIdentity from local storage
+Future<String> retrieveUser() async {
+  final prefs = await SharedPreferences.getInstance();
+  String userIdentity = prefs.getString('userIdentity') ?? "";
+
+  if (userIdentity == "") {
+    userIdentity = generateUserIdentity();
+    await prefs.setString('userIdentity', userIdentity);
+  }
+
+  return userIdentity;
+}
