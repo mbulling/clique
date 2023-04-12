feed155314bab1bf20a6fdc7cfb66cefd1656ceb
andrew <asc269@cornell.edu>
2023-04-12 19:33:40 -0400

    Define region class

---
+++ lib/backend/region.dart
@@ -0,0 +1,15 @@
+class Region {
+  final double latitude;
+  final double longitude;
+  final double radius;
+
+  Region(this.latitude, this.longitude, this.radius);
+
+  Region.fromJson(Map<String, Object> json)
+      : this(json["latitude"] as double, json["longitude"] as double,
+            json["radius"] as double);
+
+  Map<String, Object> toJson() {
+    return {"latitude": latitude, "longitude": longitude, "radius": radius};
+  }
+}
