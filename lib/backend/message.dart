2dea606ec3141182d845a97e6df879222e2aa663
andrew <asc269@cornell.edu>
2023-04-12 22:00:49 -0400

    Redefine message and region class

--- lib/backend/message.dart
+++ lib/backend/message.dart
@@ -1,63 +1,21 @@
-import 'region.dart';
-import 'dart:math';
-import 'utils.dart';
-
 class Message {
   final String text;
   final double latitude;
   final double longitude;
-  late List<Region> inRegions;
+  final String region;
 
-  Message(this.latitude, this.longitude, this.text) {
-    _initRegions(latitude, longitude);
-  }
+  Message(this.latitude, this.longitude, this.text, this.region);
 
   Message.fromJson(Map<String, Object?> json)
       : this(json["latitude"] as double, json["longitude"] as double,
-            json["text"] as String);
+            json["text"] as String, json["region"] as String);
 
   Map<String, Object?> toJson() {
     return {
       "latitude": latitude,
       "longitude": longitude,
       "text": text,
-      "inRegions": inRegions
+      "region": region,
     };
-  }
-
-  bool _inRegion(double latitude, double longitude, Region region) {
-    double otherLatitude = region.latitude;
-    double otherLongitude = region.longitude;
-    double otherRadius = region.radius;
-    double distance =
-        _distance(latitude, longitude, otherLatitude, otherLongitude);
-
-    return distance <= otherRadius;
-  }
-
-  double _distance(double lat1, double lon1, double lat2, double lon2) {
-    const double earthRadius = 6371.0; // in kilometers
-    double dLat = _toRadians(lat2 - lat1);
-    double dLon = _toRadians(lon2 - lon1);
-    double a = pow(sin(dLat / 2), 2) +
-        cos(_toRadians(lat1)) * cos(_toRadians(lat2)) * pow(sin(dLon / 2), 2);
-    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-    double distance = earthRadius * c;
-    return distance;
-  }
-
-  double _toRadians(double degree) {
-    return degree * pi / 180.0;
-  }
-
-  Future<void> _initRegions(double latitude, double longitude) async {
-    List<Region> regions = await getAllRegions();
-    List<Region> inRegions = [];
-    for (Region other in regions) {
-      if (_inRegion(latitude, longitude, other)) {
-        inRegions.add(other);
-      }
-    }
-    this.inRegions = inRegions;
   }
 }
--- lib/backend/region.dart
+++ lib/backend/region.dart
@@ -2,14 +2,20 @@ class Region {
   final double latitude;
   final double longitude;
   final double radius;
+  final String name;
 
-  Region(this.latitude, this.longitude, this.radius);
+  Region(this.latitude, this.longitude, this.radius, this.name);
 
   Region.fromJson(Map<String, Object?> json)
       : this(json["latitude"] as double, json["longitude"] as double,
-            json["radius"] as double);
+            json["radius"] as double, json["name"] as String);
 
   Map<String, Object?> toJson() {
-    return {"latitude": latitude, "longitude": longitude, "radius": radius};
+    return {
+      "latitude": latitude,
+      "longitude": longitude,
+      "radius": radius,
+      "name": name
+    };
   }
 }
