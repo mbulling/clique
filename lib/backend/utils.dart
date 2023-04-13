509abd40e4eaa64eb0b682c0a6dbe0ff5fc6c01e
andrew <asc269@cornell.edu>
2023-04-12 21:35:59 -0400

    Add reference to regions collection and get region functions

--- lib/backend/utils.dart
+++ lib/backend/utils.dart
@@ -1,6 +1,9 @@
 import 'package:clique/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'message.dart';
+import 'region.dart';
 
 //The initializeFirebase() function is used to initialize the Firebase
 //configuration in a Flutter application.
@@ -9,4 +12,21 @@ Future<void> initializeFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+}
+
+//Firestore collection references
+
+final regionsRef =
+    FirebaseFirestore.instance.collection('regions').withConverter<Region>(
+          fromFirestore: (snapshots, _) => Region.fromJson(snapshots.data()!),
+          toFirestore: (message, _) => message.toJson(),
+        );
+
+//regions collection functions
+
+Future<List<Region>> getAllRegions() async {
+  QuerySnapshot<Region> querySnapshot = await regionsRef.get();
+  List<Region> regionsList =
+      querySnapshot.docs.map((doc) => doc.data()).toList();
+  return regionsList;
 }
