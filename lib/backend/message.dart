8ecbd2569861f77c99e295aef0dd0b90dd50a164
andrew <asc269@cornell.edu>
2023-04-12 21:34:19 -0400

    Add inRegions attr to message class - gives where a message should be shown

--- lib/backend/message.dart
+++ lib/backend/message.dart
@@ -1,24 +1,63 @@
+import 'region.dart';
+import 'dart:math';
+import 'utils.dart';
+
 class Message {
-  final String author;
   final String text;
   final double latitude;
   final double longitude;
-  final String? media;
+  late List<Region> inRegions;
 
-  Message(this.author, this.text, this.latitude, this.longitude, {this.media});
+  Message(this.latitude, this.longitude, this.text) {
+    _initRegions(latitude, longitude);
+  }
 
   Message.fromJson(Map<String, Object?> json)
-      : this(json["author"] as String, json["text"] as String,
-            json["latitude"] as double, json["longitude"] as double,
-            media: json["media"] as String?);
+      : this(json["latitude"] as double, json["longitude"] as double,
+            json["text"] as String);
 
   Map<String, Object?> toJson() {
     return {
-      'author': author,
-      'text': text,
-      'latitude': latitude,
-      'longitude': longitude,
-      'media': media,
+      "latitude": latitude,
+      "longitude": longitude,
+      "text": text,
+      "inRegions": inRegions
     };
+  }
+
+  bool _inRegion(double latitude, double longitude, Region region) {
+    double otherLatitude = region.latitude;
+    double otherLongitude = region.longitude;
+    double otherRadius = region.radius;
+    double distance =
+        _distance(latitude, longitude, otherLatitude, otherLongitude);
+
+    return distance <= otherRadius;
+  }
+
+  double _distance(double lat1, double lon1, double lat2, double lon2) {
+    const double earthRadius = 6371.0; // in kilometers
+    double dLat = _toRadians(lat2 - lat1);
+    double dLon = _toRadians(lon2 - lon1);
+    double a = pow(sin(dLat / 2), 2) +
+        cos(_toRadians(lat1)) * cos(_toRadians(lat2)) * pow(sin(dLon / 2), 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    double distance = earthRadius * c;
+    return distance;
+  }
+
+  double _toRadians(double degree) {
+    return degree * pi / 180.0;
+  }
+
+  Future<void> _initRegions(double latitude, double longitude) async {
+    List<Region> regions = await getAllRegions();
+    List<Region> inRegions = [];
+    for (Region other in regions) {
+      if (_inRegion(latitude, longitude, other)) {
+        inRegions.add(other);
+      }
+    }
+    this.inRegions = inRegions;
   }
 }
